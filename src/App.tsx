891c9aea70e34cf11292312a831d84743e1733a9
chogyejin <whrpwls96@naver.com>
2022-02-20 23:03:35 +0900

    feat: add simple button with useState

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,50 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
-function App() {
+const App = () => {
+  const [item, setItem] = useState<number>(1);
+  const upItem = () => setItem(item + 1);
+  const downItem = () => setItem(item - 1);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div>{item}</div>
+      <button onClick={upItem}>UP</button>
+      <button onClick={downItem}>DOWN</button>
     </div>
   );
+};
+
+class AppUgly extends React.Component<{}, { item: number }> {
+  state = {
+    item: 1,
+  };
+
+  render() {
+    const { item } = this.state;
+    return (
+      <div>
+        <div>{item}</div>
+        <button onClick={this.upItem}>UP</button>
+        <button onClick={this.downItem}>DOWN</button>
+      </div>
+    );
+  }
+
+  upItem = () => {
+    this.setState((state) => {
+      return {
+        item: state.item + 1,
+      };
+    });
+  };
+  downItem = () => {
+    this.setState((state) => {
+      return {
+        item: state.item - 1,
+      };
+    });
+  };
 }
 
 export default App;
