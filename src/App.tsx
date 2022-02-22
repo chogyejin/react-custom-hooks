beebd982730ae67560ddb4e62a8bc98a93e1ab04
chogyejin <whrpwls96@naver.com>
2022-02-22 23:30:15 +0900

    feat: Add useTitle to change html title

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,11 @@
-import { useTabs } from "./hooks/useTabs";
-
-const content = [
-  {
-    tab: "Section 1",
-    content: "Content of Section 1",
-  },
-  {
-    tab: "Section 2",
-    content: "Content of Section 2",
-  },
-];
+import { useTitle } from "./hooks/useTitle";
 
 const App = () => {
-  // const tabs = useTabs(0, content);
-  const { currentItem, changeItem } = useTabs(0, content); // tabs.currentItem.~~ 대신 currentItem.~~ 바로 씀
+  const titleUpdater = useTitle("Loading...");
+  setTimeout(() => titleUpdater("Home"), 5000);
   return (
     <div>
-      {content.map((section, index) => (
-        <button onClick={() => changeItem(index)}>{section.tab}</button>
-      ))}
-      <div>{currentItem.content}</div>
+      <div>ㅁㄴㅁ </div>
     </div>
   );
 };
